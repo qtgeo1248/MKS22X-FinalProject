066abaf8dce2d30c5bf3de1167c2852b7249ba7c
qtgeo1248 <gzhou00@stuy.edu>
2019-05-19 03:04:46 -0400

    copy pasted code to be tested

--- WordGame/Test.java
+++ WordGame/Test.java
@@ -1,18 +1,20 @@
-class Word {
+import java.util.*;
+import java.lang.*;
+public class Test {
   String word;
   ArrayList<String> allPossWords;
-  
-  Word(String newWord) {
+
+  public Test(String newWord) {
     word = newWord;
     allPossWords = new ArrayList<String>();
   }
-  
-  void allCombos() {
+
+  public void allCombos() {
     boolean[] whichLetters = new boolean[word.length()];
     for (int i = 1; i <= (int)Math.pow(2, word.length()); i++) {
       String toAdd = "";
       for (int j = 0; j < whichLetters.length; j++) {
-        whichLetters[j] = i % (int)Math.pow(2, j) == 0;
+        whichLetters[j] = i % Math.pow(2, j) == 0;
       }
       for (int k = 0; k < whichLetters.length; k++) {
         if (whichLetters[k]) {
