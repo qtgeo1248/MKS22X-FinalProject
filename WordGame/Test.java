9637db10b3c532cefb5850a9aa9fbf37c4faf55c
qtgeo1248 <gzhou00@stuy.edu>
2019-05-19 03:06:11 -0400

    testing happy

--- WordGame/Test.java
+++ WordGame/Test.java
@@ -1,27 +1,32 @@
 import java.util.*;
 import java.lang.*;
 public class Test {
-  String word;
-  ArrayList<String> allPossWords;
+    String word;
+    ArrayList<String> allPossWords;
 
-  public Test(String newWord) {
-    word = newWord;
-    allPossWords = new ArrayList<String>();
-  }
-
-  public void allCombos() {
-    boolean[] whichLetters = new boolean[word.length()];
-    for (int i = 1; i <= (int)Math.pow(2, word.length()); i++) {
-      String toAdd = "";
-      for (int j = 0; j < whichLetters.length; j++) {
-        whichLetters[j] = i % Math.pow(2, j) == 0;
-      }
-      for (int k = 0; k < whichLetters.length; k++) {
-        if (whichLetters[k]) {
-          toAdd += word.charAt(k);
-        }
-      }
-      allPossWords.add(toAdd);
+    public Test(String newWord) {
+        word = newWord;
+        allPossWords = new ArrayList<String>();
     }
-  }
+
+    public void allCombos() {
+        boolean[] whichLetters = new boolean[word.length()];
+        for (int i = 1; i <= (int)Math.pow(2, word.length()); i++) {
+            String toAdd = "";
+            for (int j = 0; j < whichLetters.length; j++) {
+                whichLetters[j] = i % Math.pow(2, j) == 0;
+            }
+            for (int k = 0; k < whichLetters.length; k++) {
+                if (whichLetters[k]) {
+                    toAdd += word.charAt(k);
+                }
+            }
+            allPossWords.add(toAdd);
+        }
+        System.out.println(allPossWords);
+    }
+    public static void main(String[] args) {
+        Test test = new Test("happy");
+        test.allCombos();
+    }
 }
